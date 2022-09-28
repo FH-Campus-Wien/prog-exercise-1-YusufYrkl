dce4d4adaad2f4bc9526770cdf7e1d28f93bd566
cranecree <cranecree@gmail.com>
2022-09-28 20:43:05 +0200

    Update

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -53,27 +53,117 @@ public class App {
     public void addTwoNumbers(){
         // input your solution here
         Scanner sc = new Scanner(System.in);
+        int x;
+        int y;
+        int z;
 
+        x = sc.nextInt();
+        y = sc.nextInt();
+        z = x+y;
+        System.out.println(z);
     }
 
     //todo Task 5
     public void swapTwoNumbers(){
         // input your solution here
+        Scanner sc = new Scanner(System.in);
+        int x;
+        int y;
+
+        System.out.println("Before Swap:");
+        x = sc.nextInt();
+        y = sc.nextInt();
+
+        x = x + y;
+        y = x - y;
+        x = x - y;
+
+        System.out.println("After Swap:");
+        System.out.println(x +
+                     "\n"+ y );
     }
 
     //todo Task 6
     public void compareTwoNumbers(){
         // input your solution here
+        Scanner sc = new Scanner(System.in);
+        int x;
+        int y;
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("n1: ");
+            x = sc.nextInt();
+            System.out.println("n2: ");
+            y = sc.nextInt();
+
+            if (x == y) {
+                System.out.printf("n1 == n2"+ System.lineSeparator());
+
+            } else if (x > y) {
+                System.out.printf("n1 > n2"+ System.lineSeparator());
+
+            } else if (y > x) {
+                System.out.printf("n2 > n1"+ System.lineSeparator());
+
+            } else if (x < y) {
+                System.out.printf("n1 < n2"+ System.lineSeparator());
+
+            } else if (y < x) {
+                System.out.println("n2 > n1"+ System.lineSeparator());
+            }
+        }
     }
 
     //todo Task 7
-    public void ratingSalesPerson(){
+    public void ratingSalesPerson() {
         // input your solution here
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter annual Revenue");
+        int x = sc.nextInt();
+
+        for (int i = 1; i <= 2; i++) {
+
+            if (0 <= x || x >= 100000) {
+                System.out.printf("Invalid Revenue" + System.lineSeparator());
+
+            } else if (x <= 0 || x < 20000) {
+                System.out.printf("Poor Sales Revenue" + System.lineSeparator());
+
+            } else if (x <= 50000 || x < 80000) {
+                System.out.printf("Average Sales Revenue" + System.lineSeparator());
+
+            } else if (x <= 80000 || x < 100000) {
+                System.out.printf("Excellent Sales Revenue" + System.lineSeparator());
+            }
+
+        }
     }
+
 
     //todo Task 8
     public void getCommissionRate(){
-        // input your solution here
+        // input your solution here#
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter CommissionClass: ");
+        double x = sc.nextDouble();
+
+        switch (x){
+            case 0.01:
+                System.out.println("Your Commission Rate was set to 0.01");
+                break;
+            case 0.02:
+                System.out.println("Your Commission Rate was set to 0.02");
+                break;
+            case 0.03:
+                System.out.println("Your Commission Rate was set to 0.03");
+                break;
+            case 0.04:
+                System.out.println("Your Commission Rate was set to 0.04");
+                break;
+            default:
+                System.out.println("Your Commission Rate was set to 0.00");
+        }
     }
 
     //todo Task 9
